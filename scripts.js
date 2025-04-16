a9b8ca2cb8c9635b358af6911944bb3e399ae5f7
belavelle <162368112+belavelle@users.noreply.github.com>
2025-04-16 13:52:48 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,3 @@
-document.getElementById('serverConfigForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const operatingSystem = document.getElementById('operatingSystem').value;
-    const azureShape = document.getElementById('azureShape').value;
-    alert(`Operating System: ${operatingSystem}\nAzure Shape: ${azureShape}`);
-});
-
 document.getElementById('addDiskButton').addEventListener('click', function() {
     const diskSection = document.getElementById('diskSection');
     
